f89e473747d0a5a424d18dd5f14eec622f610a20
muniuni <rom.undertale@gmail.com>
2025-04-28 22:08:37 +0900

    fix import syntax

--- src/app/api/projects/[id]/route.ts
+++ src/app/api/projects/[id]/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/projects/[id]/route.ts
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import {
