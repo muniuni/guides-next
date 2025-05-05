670987b9c35d4937f18dddd26cb658e5759efe3c
muniuni <rom.undertale@gmail.com>
2025-05-05 13:26:07 +0900

    move speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Providers>
             <Header />
             {children}
-            <SpeedInsights />
           </Providers>
         </AppRouterCacheProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
